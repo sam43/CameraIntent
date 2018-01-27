784ff071b2478fd8653e41cf09c572dbfb0227e9
sam43 <sayemruet.103043@gmail.com>
2018-01-27 22:55:10 +0600

    First Camera Intent commit

---
+++ app/src/main/java/com/example/looser43/cameraintent/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.looser43.cameraintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+
+public class MainActivity extends Activity {
+
+
+    private static final int CAMERA_REQUEST = 1111;
+    private ImageView imageView;
+    private Bitmap bm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        Button photoButton = (Button) findViewById(R.id.button1);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            bm = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bm);
+            //bm = BitmapFactory.decodeFile(imagePath);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+            byte[] byteArrayImage = baos.toByteArray();
+
+            String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+
+            Log.d("Base64", "convertToBase64: " + encodedImage);
+
+        }
+    }
+
+    // convert from image path
+
+    private String convertToBase64(String imagePath)
+
+    {
+
+        bm = BitmapFactory.decodeFile(imagePath);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        byte[] byteArrayImage = baos.toByteArray();
+
+        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+
+        Log.d("Base64", "convertToBase64: " + encodedImage);
+
+        return encodedImage;
+
+    }
+}
